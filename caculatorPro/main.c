53bf72c7f6d8a8769919774ef661a368f830e946
AetherNET65535 <aethernet65535@gmail.com>
2025-03-04 11:40:00 +0800

    Auto push: 2025-03-04 11:40:00

--- caculatorPro/main.c
+++ caculatorPro/main.c
@@ -2,3 +2,32 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+// 全局变量
+char* input;
+
+// 函数声明
+int parse_expr();
+int parse_term();
+int parse_factor();
+
+void skip_space()
+{
+    if (*input == ' ' || *input == '\t')
+    {
+        input++;
+    }
+}
+
+int main ()
+{
+    char box [1024];
+    printf ("请输入表达式（必须使用显式乘法）：\n");
+    if (fgets(box, sizeof(box), stdin) == NULL)
+    {
+        printf ("看到这条表示出BUG了！！ [获取用户输入字符串]\n");
+        return 0;
+    }
+
+    input = box;
+    int result = parse_expr(); 
+}
