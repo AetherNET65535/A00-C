881889c4bad7612d86f6b496296c33739c19a148
AetherNET65535 <aethernet65535@gmail.com>
2025-03-09 04:20:00 +0800

    Auto push: 2025-03-09 04:20:00

--- memory/main.c
+++ memory/main.c
@@ -7,6 +7,21 @@
 #define MEMEOWRY_SIZE 1024 * 1 //1024 * x = xKB
 #define MEMEOWRY_MIN_SIZE sizeof(memeowry_block_header)
 
+// 内存块使用状态
+typedef enum
+{
+    USED = 0,
+    FREE = 1
+} Block_Status;
+
+typedef struct memeowry_block_header
+{
+    size_t size;
+    Block_Status status;
+
+    struct memeowry_block_header *prev;
+    struct memeowry_block_header *next;
+} memeowry_block_header
 
 int main ()
 {
