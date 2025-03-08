eb0288074432a76081b4e42383a3a55e9cc95fc5
AetherNET65535 <aethernet65535@gmail.com>
2025-03-09 04:00:00 +0800

    Auto push: 2025-03-09 04:00:00

--- memory/main.c
+++ memory/main.c
@@ -2,3 +2,13 @@
 #include <stddef.h>
 #include <string.h>
 
+// MEMEOWRY == MEMORY, BUT CUTER~ MEOW~
+// 内存条（块）总容量 和 最小容量
+#define MEMEOWRY_SIZE 1024 * 1 //1024 * x = xKB
+#define MEMEOWRY_MIN_SIZE sizeof(memeowry_block_header)
+
+
+int main ()
+{
+
+}
